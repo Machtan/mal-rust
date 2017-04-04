b2d0b721c418920eccd20a61ee7f0a74916e468d
Machtan <jako3047@gmail.com>
2017-04-04 13:31:28 +0200

    Implement let*

--- src/step3_env.rs
+++ src/step3_env.rs
@@ -50,23 +50,45 @@ fn eval_list(list: &mut MalList, env: &mut Env) -> mal::Result<()> {
     Ok(())
 }
 
+fn assert_arg_len(name: &str, nargs: usize, args: &MalList) -> mal::Result<()> {
+    if args.len() != nargs {
+        bail!("'{}' takes {} arguments, found {}", name, nargs, args.len());
+    }
+    Ok(())
+}
+
 fn apply(list: &mut MalList, env: &mut Env) -> mal::Result<Mal> {
     let first = list.pop_front().unwrap().symbol()?;
     match first.text() {
         "def!" => {
-            let sym = list.pop_front()
-                .ok_or_else(|| Error::from("def!: missing arg 1 (symbol)"))
-                .and_then(|mal| {
-                    mal.symbol().chain_err(|| "def!: Invalid first argument")
-                })?;
-            let mut val = list.pop_front()
-                .ok_or_else(|| Error::from("def! missing arg 2 (value)"))?;
+            assert_arg_len("def!", 2, list)?;
+            let sym = list.pop_front().unwrap().symbol()
+                .chain_err(|| "def!: Invalid first argument")?;
+            let mut val = list.pop_front().unwrap();
             eval(&mut val, env)?;
-            if ! list.is_empty() {
-                bail!("def!: called with more than 2 args!");
-            }
             env.set(sym, val);
             Ok(Mal::Nil)
+        }
+        "let*" => {
+            assert_arg_len("let*", 2, list)?;
+            let mut bindings = list.pop_front().unwrap().list()
+                .chain_err(|| "let*: Invalid list of bindings")?;
+            if (bindings.len() % 2) != 0 {
+                bail!("let*: odd number of elements in binding list");
+            }
+            
+            env.with_new_scope(|env| {
+                while ! bindings.is_empty() {
+                    let sym = bindings.pop_front().unwrap().symbol()
+                        .chain_err(|| "let*: Invalid variable")?;
+                    let mut val = bindings.pop_front().unwrap();
+                    eval(&mut val, env)?;
+                    env.set(sym, val);
+                }
+                let mut expr = list.pop_front().unwrap();
+                eval(&mut expr, env)?;
+                Ok(expr)
+            })
         }
         _ => {
             let func = env.get(&first)?;
